95ff576300caa8a388ae24a8962a765f0a32759c
SolidRisk-bit <solucionesfinancieras.larrauri@gmail.com>
2026-08-05 18:51:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -829,6 +829,7 @@ with st.sidebar:
             st.session_state.modulo = "Dashboard Ejecutivo"
             st.rerun()
 
+css = f"""
 section[data-testid="stSidebar"] div.stButton:last-child > button {{
     background: #6B7280; /* Gris */
     border: 1px solid #4B5563;
@@ -838,6 +839,7 @@ section[data-testid="stSidebar"] div.stButton:last-child > button:hover {{
     background: #4B5563; /* Gris más oscuro */
     border: 1px solid #374151;
 }}
+"""
 
 # =====================================================
 # DASHBOARD EJECUTIVO
